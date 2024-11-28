e7301ccfb9141b5eb83eb82f468f52225152e0c4
wjensen1985 <william.hw.jensen@gmail.com>
2024-11-27 21:27:39 -0500

    pwm light cycle

---
+++ app/src/core/timer.c
@@ -0,0 +1,33 @@
+#include "core/timer.h"
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/rcc.h>
+
+// 84 000 000
+// freq = sys_freq / ((prescaler-1) + (auto_reload_reg-1))
+#define PRESCALER   (84)
+#define ARR_VALUE   (1000)
+
+void timer_setup(void){
+    rcc_periph_clock_enable(RCC_TIM2);
+
+    // high lvl timer config
+    timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+
+    // set up pwm
+    timer_set_oc_mode(TIM2, TIM_OC1, TIM_OCM_PWM1);
+
+    // enable pwm output
+    timer_enable_counter(TIM2);
+    timer_enable_oc_output(TIM2, TIM_OC1);
+
+    // set up freq + resolution
+    timer_set_prescaler(TIM2, PRESCALER-1);
+    timer_set_period(TIM2, ARR_VALUE - 1);
+}
+
+void timer_pwm_set_duty_cycle(float duty_cycle){
+    // duty_cycle = (cap_compare_reg / arr) * 100
+    // you set ccr value in register;  ccr = (duty_cycle / 100) * arr
+    const float raw_value = (float)ARR_VALUE * duty_cycle / 100.0f;
+    timer_set_oc_value(TIM2, TIM_OC1, (uint32_t)raw_value);
+}
--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,28 +1,43 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+
 #include "core/system.h"
+#include "core/timer.h"
 
 #define LED_PORT   (GPIOA)
 #define LED_PIN    (GPIO5)
-#define BLINK_FREQ (100)
 
 static void gpio_setup(void){
   rcc_periph_clock_enable(RCC_GPIOA);
-  gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+  gpio_mode_setup(LED_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, LED_PIN);
+  gpio_set_af(LED_PORT, GPIO_AF1, LED_PIN);
 }
 
 int main(void){
   system_setup();
   gpio_setup();
+  timer_setup();
 
   uint64_t start_time = system_get_ticks();
   uint64_t last_blink = start_time;
+
+  float duty_cycle = 0.0f;
+  timer_pwm_set_duty_cycle(duty_cycle);
   
   while(1){
-    if (system_get_ticks() - last_blink >= BLINK_FREQ) {
-      gpio_toggle(LED_PORT, LED_PIN);
+
+    // led blink check
+    if (system_get_ticks() - last_blink >= 10) {
+      duty_cycle += 1.0f;
+      if (duty_cycle > 100.0f){
+        duty_cycle = 0.0f;
+      }
+      timer_pwm_set_duty_cycle(duty_cycle);
+      
       last_blink = system_get_ticks();
     }
+
+
 
   }
 
